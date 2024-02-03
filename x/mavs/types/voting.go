32925cf1487ad37f81ae310fe5fe62bf7db943cf
Emanuel <emaa.forlin@gmail.com>
2024-02-03 14:42:50 -0300

    Adds some helper functions

--- x/mavs/types/voting.go
+++ x/mavs/types/voting.go
@@ -1,9 +1,7 @@
 package types
 
 import (
-	"fmt"
-	"strings"
-	"time"
+	"github.com/emaforlin/mAVS/x/mavs/directives"
 )
 
 func (s StoredVoting) CountVotes() (votes uint64) {
@@ -17,48 +15,29 @@ func (s StoredVoting) CountVotes() (votes uint64) {
 func (s StoredVoting) GetVotesFrom(partyName string) (res uint64, err error) {
 	res, found := s.Counting[partyName]
 	if !found {
-		err = ErrCandidateNotFound
-		return
+		return 0, ErrCandidateNotFound
 	}
-	return
+	return res, nil
 }
 
-func (s StoredVoting) StillActive() (err error) {
-	tw := strings.Split(s.Timewindow, " ")
-	for i, j := range tw {
-		year := j[:4]
-		month := j[4:6]
-		day := j[6:8]
-		hours := j[8:10]
-		minutes := j[10:12]
-		seconds := j[12:]
-		var dateSep string = "-"
-		var timeSep string = ":"
-		tw[i] = year + dateSep + month + dateSep + day + " " + hours + timeSep + minutes + timeSep + seconds
-	}
-
-	start, err := time.Parse(time.DateTime, tw[0])
-	if err != nil {
-		fmt.Printf("Raw Time: %v\nStart time %v\n", tw[0], start)
-		return err
-	}
-	end, err := time.Parse(time.DateTime, tw[1])
-	if err != nil {
-		return err
-	}
-	if end.Sub(start) < time.Hour {
-		return ErrVotingTimeTooShort
-	}
-	if end.Before(time.Now()) {
-		return ErrVotationEnded
-	}
-	return nil
+func (s StoredVoting) ParseVoting() (voting *directives.VotingImpl, err error) {
+	return directives.Parse(s.Title, s.Timewindow, &s.Counting), nil
 }
 
-func (s StoredVoting) Validate() (err error) {
-	if s.Index == "" {
+func (s StoredVoting) Validate() error {
+	v, err := s.ParseVoting()
+	if v.Title == "" {
 		return ErrBadStoredVoting
 	}
+	if !v.StillActive() {
+		return ErrVotationEnded
+	}
+	if len(v.VoteCount) <= 1 {
+		return ErrNotEnoughCandidates
+	}
 
+	if err != nil {
+		return err
+	}
 	return nil
 }
