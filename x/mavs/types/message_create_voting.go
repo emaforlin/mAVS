8896be065a2a1fbe2ce461159b1fcbfabeed7cf7
Emanuel <emaa.forlin@gmail.com>
2024-02-03 18:02:17 -0300

    check input sanity

--- x/mavs/types/message_create_voting.go
+++ x/mavs/types/message_create_voting.go
@@ -1,9 +1,12 @@
 package types
 
 import (
+	"strings"
+
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/emaforlin/mAVS/x/mavs/directives"
 )
 
 var _ sdk.Msg = &MsgCreateVoting{}
@@ -21,6 +24,15 @@ func (msg *MsgCreateVoting) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
+	if !directives.VotingTimeFromString(msg.Timewindow).StillActive() {
+		return ErrVotationEnded
+	}
+	if len(strings.Split(msg.Candidates, " ")) <= 1 {
+		return ErrNotEnoughCandidates
+	}
+	if msg.Title == "" {
+		return ErrBadStoredVoting
 	}
 	return nil
 }
--- x/mavs/types/message_create_voting_test.go
+++ x/mavs/types/message_create_voting_test.go
@@ -1,30 +1,56 @@
-package types
+package types_test
 
 import (
 	"testing"
 
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/emaforlin/mAVS/testutil/sample"
+	"github.com/emaforlin/mAVS/x/mavs/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMsgCreateVoting_ValidateBasic(t *testing.T) {
 	tests := []struct {
 		name string
-		msg  MsgCreateVoting
+		msg  types.MsgCreateVoting
 		err  error
 	}{
 		{
-			name: "invalid address",
-			msg: MsgCreateVoting{
-				Creator: "invalid_address",
+			name: "invalid creator address",
+			msg: types.MsgCreateVoting{
+				Creator:    "invalid_address",
+				Title:      "valid_title",
+				Candidates: "candidate list separated by a space like this",
+				Timewindow: "30991110080000 30991110140000", //time window valid for a while
 			},
 			err: sdkerrors.ErrInvalidAddress,
 		}, {
-			name: "valid address",
-			msg: MsgCreateVoting{
-				Creator: sample.AccAddress(),
+			name: "votation ended",
+			msg: types.MsgCreateVoting{
+				Creator:    sample.AccAddress(),
+				Title:      "This voting has already ended",
+				Candidates: "candidate1 candidate2 candidate3",
+				Timewindow: "20130821120000 20130821233000", //time window caducated in 2013
 			},
+			err: types.ErrVotationEnded,
+		}, {
+			name: "invalid title",
+			msg: types.MsgCreateVoting{
+				Creator:    sample.AccAddress(),
+				Title:      "",
+				Candidates: "candidate1 candidate2 candidate3",
+				Timewindow: "30991110080000 30991110140000",
+			},
+			err: types.ErrBadStoredVoting,
+		}, {
+			name: "empty candidate list",
+			msg: types.MsgCreateVoting{
+				Creator:    sample.AccAddress(),
+				Title:      "candidate list empty",
+				Candidates: "",
+				Timewindow: "30991110080000 30991110140000",
+			},
+			err: types.ErrNotEnoughCandidates,
 		},
 	}
 	for _, tt := range tests {
